154fa15900f0f55076d580a7dc588161e19f4cc5
Sergey Vedmedev <vedmedev@trueconf.ru>
2019-05-22 17:15:02 +0300

    import fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {store} from './store.js'
-import lightbox from 'vlightbox'
 
 Vue.config.productionTip = false
-
-Vue.use(lightbox)
 
 new Vue({
   store,
